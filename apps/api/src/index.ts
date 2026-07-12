0838e5a081877807fadc54bea3da3615c3907bd7
rohitJung <rokshh28@gmail.com>
2026-07-12 22:21:16 +0545

    feat: cors and env example

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,26 +1,29 @@
-import express from "express";
-import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter, createContext } from "@abhimanyu/trpc/server";
-import { auth } from "@abhimanyu/services";
-import { toNodeHandler } from "better-auth/node";
-import cors from "cors";
+import "dotenv/config"
+import express from "express"
+import { createExpressMiddleware } from "@trpc/server/adapters/express"
+import { appRouter, createContext } from "@abhimanyu/trpc/server"
+import { auth } from "@abhimanyu/services"
+import { toNodeHandler } from "better-auth/node"
+import cors from "cors"
 
-const app = express();
+const app = express()
 
 // CORS
 app.use(
   cors({
-    origin: ["*"], // allow all  for now
+    origin: process.env.ALLOWED_ORIGINS
+      ? process.env.ALLOWED_ORIGINS.split(",")
+      : ["*"], // allow all  for now
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true, // cookies, authorization headers, etc
-  }),
-);
+  })
+)
 
 // better auth for authentication
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all("/api/auth/*splat", toNodeHandler(auth))
 
 // after better auth
-app.use(express.json());
+app.use(express.json())
 
 // trpc for other routes
 app.use(
@@ -28,11 +31,11 @@ app.use(
   createExpressMiddleware({
     router: appRouter,
     createContext,
-  }),
-);
+  })
+)
 
-const port = process.env.PORT ?? 4000;
+const port = process.env.PORT ?? 4000
 
 app.listen(port, () => {
-  console.log("app is listening at", 4000);
-});
+  console.log("app is listening at", 4000)
+})
