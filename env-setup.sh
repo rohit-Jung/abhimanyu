7f8cd112282666abd4ee19b5b6b7d013c28afe4a
rohitJung <rokshh28@gmail.com>
2026-07-17 22:19:34 +0545

    feat: env setup script

---
+++ env-setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ -f ".env" ]]; then
+  echo ".env file exists"
+else
+  echo ".env file does not exists"
+  cp .env.example .env
+fi
+
+for dir in apps/* packages/*; do
+  if [[ -d "$dir" ]]; then
+    target="$dir/.env"
+
+    if [[ ! -L "$target" ]] || [[ "$(readlink -- "$target")" != "$(realpath ".env")}" ]]; then
+      if [[ ! -e "$target" ]]; then
+        link .env "$target"
+      fi
+    fi
+  fi
+done
