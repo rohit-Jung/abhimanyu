24094ddc20a26c1e71982c00db89d148af231751
rohitJung <rokshh28@gmail.com>
2026-07-20 21:41:55 +0545

    fix: exports and imports

--- packages/trpc/server/trpc.ts
+++ packages/trpc/server/trpc.ts
@@ -1,6 +1,6 @@
 import { initTRPC, TRPCError } from "@trpc/server"
 
-import { type Context } from "./expressContext"
+import { type Context } from "./context"
 
 // trpc initialization
 export const t = initTRPC.context<Context>().create()
