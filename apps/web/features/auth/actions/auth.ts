6d7e7448d59c06bb217cc1e2a4dd0532127e54d3
rohitJung <rokshh28@gmail.com>
2026-07-20 21:25:09 +0545

    feat: cache user session

--- apps/web/features/auth/actions/auth.ts
+++ apps/web/features/auth/actions/auth.ts
@@ -3,19 +3,20 @@
 import { auth } from "@abhimanyu/services"
 import { headers } from "next/headers"
 import { redirect } from "next/navigation"
+import { cache } from "react"
 
 import { AUTH_ROUTES } from "@/lib/constants"
 
-export async function getUserSession(
-  redirectRoute: AUTH_ROUTES = AUTH_ROUTES.signup
-) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
+export const getUserSession = cache(
+  async (redirectRoute: AUTH_ROUTES = AUTH_ROUTES.signup) => {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    })
 
-  if (!session) {
-    redirect(redirectRoute)
+    if (!session) {
+      redirect(redirectRoute)
+    }
+
+    return session
   }
-
-  return session
-}
+)
