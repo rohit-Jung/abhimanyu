dd41b8a21b2e4c284bb58e9da1006c5126cdf8fa
rohitJung <rokshh28@gmail.com>
2026-07-27 20:55:12 +0545

    feat: add githubRepolist

--- apps/web/app/(main)/dashboard/repos/page.tsx
+++ apps/web/app/(main)/dashboard/repos/page.tsx
@@ -8,6 +8,8 @@ import {
 import { Separator } from "@abhimanyu/ui/components/separator"
 import { SidebarInset, SidebarTrigger } from "@abhimanyu/ui/components/sidebar"
 
+import GithubRepos from "@/components/github/github-repos"
+
 export default async function Page() {
   return (
     <SidebarInset>
@@ -26,14 +28,7 @@ export default async function Page() {
           </BreadcrumbList>
         </Breadcrumb>
       </header>
-      <div className="flex w-full flex-1 flex-col gap-4 p-4">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
+      <GithubRepos />
     </SidebarInset>
   )
 }
