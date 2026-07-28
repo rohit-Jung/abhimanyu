bbe242c7615cf51178c1026cee64ffab3e460621
rohitJung <rokshh28@gmail.com>
2026-07-28 22:21:50 +0545

    feat: add reposync schema

--- packages/contracts/src/github/github-reposync.schema.ts
+++ packages/contracts/src/github/github-reposync.schema.ts
@@ -17,5 +17,24 @@ export const gitRepo = type({
   syncStatus: type("'Pending' | 'Synced' | 'Failed' | 'Syncing' | null"),
 })
 
+export const syncRepoDetails = type({
+  repoFullName: "string",
+  userId: "string",
+  branch: "string",
+})
+
+export interface RepoFile {
+  filePath: string
+  content: string
+}
+
+export interface TreeEntry {
+  path?: string
+  type?: string
+  sha?: string
+  size?: number
+}
+
 export type GetInfiniteRepo = typeof getInfiniteRepoSchema.infer
+export type SyncRepoDetails = typeof syncRepoDetails.infer
 export type GitRepo = typeof gitRepo.infer
