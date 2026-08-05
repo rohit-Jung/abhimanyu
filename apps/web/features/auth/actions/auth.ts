542d3e25bf05e12777b838ce7908a42a387d5d8f
rohitJung <rokshh28@gmail.com>
2026-08-05 21:54:31 +0545

    feat(auth): user settings action

--- apps/web/features/auth/actions/auth.ts
+++ apps/web/features/auth/actions/auth.ts
@@ -1,14 +1,16 @@
 "use server"
 
 import { auth } from "@abhimanyu/services"
+import { revalidatePath } from "next/cache"
 import { headers } from "next/headers"
 import { redirect } from "next/navigation"
 import { cache } from "react"
 
 import { AUTH_ROUTES } from "@/lib/constants"
+import { createServerCaller } from "@/lib/trpc/server"
 
 export const getUserSession = cache(
-  async (redirectRoute: AUTH_ROUTES = AUTH_ROUTES.signup) => {
+  async (redirectRoute: AUTH_ROUTES = AUTH_ROUTES.signin) => {
     const session = await auth.api.getSession({
       headers: await headers(),
     })
@@ -20,3 +22,9 @@ export const getUserSession = cache(
     return session
   }
 )
+
+export const getUserSettings = async () => {
+  const api = await createServerCaller()
+  const settings = await api.user.getUserSettings()
+  return settings
+}
